8bf0398f41a5957be87afb28251ad9f1236c6476
kravlin <kravlin@gmail.com>
2018-08-14 19:49:42 -0600

    v1.0 release

--- Badge_Software/Badge_Software.ino
+++ Badge_Software/Badge_Software.ino
@@ -8,17 +8,9 @@ int fadeAmount = 5;
 int mode = 0;
 int ledPins [8] = {3, 1, 0, 4, 6, 7, 8, 9};
 int visualizer = 0;
-int visualizerStep = 0;
+int visualizerStep = -1;
 int visualizerDir = 1;
 int slowFill = 0;
-int challengeONE = 0;
-int letterLookup('a','b','c','d','e','d','f','g','h
-int ONEmsg[] = {{1,1,0,1,0,1,1,0,1},{0,0,1,0,1,1,0,0,1,1,0,1,0,0,1,1,0,1,0,1,1,0,1,1,0,0,1,1,0,1,1,0,1,1,0,0,1,0,1,0,1,1,0,0,1,1,0,0,1,0,0,1,0,1,1,0,1,0,1,0,0,1,0,1,1,0,1,0,1,0,0,1,1,0,1,1,0,0,1,0,0,1,0,1,1,0,1,1,0,0,1,0,1,0,1,0,1,0,0,1,0,0,1,0,1,1,0,1,0,0,1,0,0,1,1,0,0,1,0,1,0,1,0,1,0,0,1,0,0,1,1,0,1,0,1,0,1,0,0,1,0,1,1,0,0,1,1,0,0,1,0,1,0,1,0,1,0,0,1,0,1,1,0,1,0,0,1,1,0,1,1,0,1,1,0,0,1,1,0,1,1,0,1,1,0,0,1,1,0,1,1,0,0,1,0,1,0,0,1,0,1,0,1,1,0,1,0,1,1,0,1,1,0,1,0,1};
-int challengeTWO = 1;
-int challengeTHREE = 0;
-int challengeFOUR = 1;
-
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -62,71 +54,86 @@ void cycleVisualizer(){
   for (int i=0; i<8; i++){
     digitalWrite(ledPins[i], LOW);
   }
-  visualizerStep = 0;
+  visualizerStep = -1;
   visualizerDir = 1;
 }
 
 //////VISUALIZERS GO HERE//////
 
 void fillDown() {
-  if(visualizerDir == -1){
-    digitalWrite(ledPins[visualizerStep], LOW);
-  }else{
-    digitalWrite(ledPins[visualizerStep], HIGH);
-  }
   visualizerStep = visualizerStep + visualizerDir;
-  if(visualizerStep <= -1 || visualizerStep >= 8){
+  if(visualizerStep > -1 && visualizerStep < 8){
+    if(visualizerDir == 1){
+      digitalWrite(ledPins[visualizerStep], HIGH);
+    }else{
+      digitalWrite(ledPins[visualizerStep], LOW);
+    }
+  }
+  else{
     visualizerDir = -visualizerDir;
   }
 }
 
 void fillUp(){
-  if(visualizerDir == 1){
-    digitalWrite(ledPins[7-visualizerStep], HIGH);
-  }else{
-    digitalWrite(ledPins[7-visualizerStep], LOW);
-  }
   visualizerStep = visualizerStep + visualizerDir;
-  if(visualizerStep <= -1 || visualizerStep >= 8){
+  if(visualizerStep > -1 && visualizerStep < 8){
+    if(visualizerDir == 1){
+      digitalWrite(ledPins[7-visualizerStep], HIGH);
+    }else{
+      digitalWrite(ledPins[7-visualizerStep], LOW);
+    }
+  }
+  else{
     visualizerDir = -visualizerDir;
   }
 }
 
 void traceLight(){
-  if(visualizerDir == -1){
-    digitalWrite(ledPins[visualizerStep--], LOW);
-    digitalWrite(ledPins[visualizerStep], HIGH);
+  if(visualizerStep == -1){
+     digitalWrite(ledPins[++visualizerStep], HIGH);   
   }else{
-    digitalWrite(ledPins[visualizerStep++], LOW);
-    digitalWrite(ledPins[visualizerStep], HIGH);
-  }
-  if(visualizerStep <= 0 || visualizerStep >= 7){
-    visualizerDir = -visualizerDir;
+    if(visualizerDir == 1){
+      digitalWrite(ledPins[visualizerStep++], LOW);
+      digitalWrite(ledPins[visualizerStep], HIGH);
+    }else{
+      digitalWrite(ledPins[visualizerStep--], LOW);
+      digitalWrite(ledPins[visualizerStep], HIGH);
+    }
+    if(visualizerStep <= 0 || visualizerStep >= 7){
+      visualizerDir = -visualizerDir;
+    }   
   }
 }
 
 void traceDark(){
-  if(visualizerDir == -1){
-    digitalWrite(ledPins[visualizerStep--], HIGH);
-    digitalWrite(ledPins[visualizerStep], LOW);
+  //This is a setup for doing this well, instead of poorly.
+  if(visualizerStep == -1){
+    for(int i= 1; i <= 7; i++){
+      digitalWrite(ledPins[i], HIGH);
+    }
+    visualizerStep++;
   }else{
-    digitalWrite(ledPins[visualizerStep++], HIGH);
-    digitalWrite(ledPins[visualizerStep], LOW);
-  }
-  if(visualizerStep <= 0 || visualizerStep >= 7){
-    visualizerDir = -visualizerDir;
+    if(visualizerDir == 1){
+      digitalWrite(ledPins[visualizerStep++], HIGH);
+      digitalWrite(ledPins[visualizerStep], LOW);
+    }else{
+      digitalWrite(ledPins[visualizerStep--], HIGH);
+      digitalWrite(ledPins[visualizerStep], LOW);
+    }
+    if(visualizerStep <= 0 || visualizerStep >= 7){
+      visualizerDir = -visualizerDir;
+    }
   }
 }
 
 void expand(){
-  if(visualizerDir == -1){
-    digitalWrite(ledPins[3-visualizerStep], LOW);
-    digitalWrite(ledPins[4+visualizerStep], LOW);
-    visualizerStep--;
-  }else{
+  visualizerStep = visualizerStep + visualizerDir;
+  if(visualizerDir == 1){
     digitalWrite(ledPins[3-visualizerStep], HIGH);
     digitalWrite(ledPins[4+visualizerStep], HIGH);
-    visualizerStep++;
+  }else{
+    digitalWrite(ledPins[3-visualizerStep], LOW);
+    digitalWrite(ledPins[4+visualizerStep], LOW);
   }
   if(visualizerStep <= -1 || visualizerStep >= 4){
     visualizerDir = -visualizerDir;
@@ -134,14 +141,13 @@ void expand(){
 }
 
 void contract(){
-  if(visualizerDir == -1){
-    digitalWrite(ledPins[visualizerStep], LOW);
-    digitalWrite(ledPins[7-visualizerStep], LOW);
-    visualizerStep--;
-  }else{
+  visualizerStep = visualizerStep + visualizerDir;
+  if(visualizerDir == 1){
     digitalWrite(ledPins[visualizerStep], HIGH);
     digitalWrite(ledPins[7-visualizerStep], HIGH);
-    visualizerStep++;
+  }else{
+    digitalWrite(ledPins[visualizerStep], LOW);
+    digitalWrite(ledPins[7-visualizerStep], LOW);
   }
   if(visualizerStep <= -1 || visualizerStep >= 4){
     visualizerDir = -visualizerDir;
@@ -149,6 +155,9 @@ void contract(){
 }
 
 void slowfillUp(){
+  if(visualizerStep == -1){
+    visualizerStep++;
+  }
   if(visualizerStep < 8){
     if(slowFill > 0){
       digitalWrite(ledPins[slowFill-1], LOW);
@@ -179,75 +188,6 @@ void slowfillUp(){
   }
 }
 
-/////////GAME CODE//////////////////
-
-void showStatus(){
-  clearLED();
-  if(challengeONE == 1){
-    digitalWrite(ledPins[0], HIGH);
-    digitalWrite(ledPins[1], HIGH);    
-  }
-  if(challengeTWO == 1){
-    digitalWrite(ledPins[2], HIGH);
-    digitalWrite(ledPins[3], HIGH);    
-  }
-  if(challengeTHREE == 1){
-    digitalWrite(ledPins[4], HIGH);
-    digitalWrite(ledPins[5], HIGH);    
-  }
-  if(challengeFOUR == 1){
-    digitalWrite(ledPins[6], HIGH);
-    digitalWrite(ledPins[7], HIGH);
-  }
-}
-
-void displayCode(int message[], int arrayLength){
-  clearLED();
-  int LEDs[] = {0,0,0,0,0,0,0,0};
-  for(int i = 0; i < arrayLength; i++){
-    for(int j = 7; j >= 0; j--){
-      if(j != 0){
-        LEDs[j] = LEDs[j-1];        
-      }else{
-        LEDs[j] = message[i];
-      }
-    }
-    for(int j = 0; j < 8; j++){
-      if(LEDs[j] == 1){
-//        digitalWrite(ledPins[j], HIGH);        
-      }else{
-//        digitalWrite(ledPins[j], LOW); 
-      }
-    }
-    delay(50);
-  }
-}
-
-void gameONE(){
-  clearLED();
-  for(int i = 0; i < 225; i++){
-    for(int j = 0; i < 8; i++){
-      if(ONEmsg[i+j] == 1){
-        digitalWrite(ledPins[j], HIGH);
-      }else{
-        digitalWrite(ledPins[j], LOW); 
-      }
-    }
-  delay(50);
-  }
-}
-
-void gameTWO(){
-  
-}
-
-void gameTHREE(){
-  
-}
-
-void gameFOUR(){
-  
-}
 
 /////EYE CODE///////////////////////
 
@@ -268,29 +208,14 @@ void clearLED(){
 }
 
 void checkButton(){
-      int buttonPushed = digitalRead(buttonPin);
-      if(buttonPushed == HIGH){
-        buttonState++;
-      }
-      if(buttonPushed == LOW && buttonState > 0 && buttonState <= 40){
-        cycleVisualizer();
-        buttonState = 0;
-      }
-      else if(buttonState > 40){
-        showStatus();
-        delay(1500);
-        clearLED();
-        if(challengeONE == 0){
- //         gameONE();
-        }else if(challengeTWO == 0){
-          gameTWO();
-        }else if(challengeTHREE == 0){
-          gameTHREE();
-        }else if(challengeFOUR == 0){
-          gameFOUR();
-        }
-        buttonState = 0;
-      }
+  int buttonPushed = digitalRead(buttonPin);
+  if(buttonPushed == HIGH){
+    buttonState++;
+  }
+  if(buttonPushed == LOW && buttonState > 0){
+    cycleVisualizer();
+    buttonState = 0;
+  }
 }
 
 void loop() {
